6dfaecc83ac4d10f608ea470d9edefb59df3d317
JLC <jean-luc.charles@ensam.eu>
2021-05-09 19:22:06 +0200

    Add write_image_file.py

--- get_image.py
+++ get_image.py
@@ -1,15 +1,16 @@
 import cv2, rospy
+import matplotlib.pyplot as plt
+import matplotlib
 from poppy_controllers.srv import GetImage
 from cv_bridge import CvBridge
+matplotlib.use('TkAgg')
 
-i=1
-while True:
-    get_image = rospy.ServiceProxy("get_image", GetImage)
-    response  = get_image()
-    bridge    = CvBridge()
-    image     = bridge.imgmsg_to_cv2(response.image)
-    cv2.imwrite(f"image{i:03d}.png", image)
-    cv2.imshow("Poppy camera", image)
-    cv2.waitKey(0)
-    i += 1
+get_image = rospy.ServiceProxy("get_image", GetImage)
+response  = get_image()
+bridge    = CvBridge()
+image     = bridge.imgmsg_to_cv2(response.image)
+plt.figure()
+plt.imshow(image)
+plt.show()
+cv2.imwrite("image.png", image)
 
