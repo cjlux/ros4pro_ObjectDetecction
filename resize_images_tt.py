4b2730250d5340f83cc505c00ba3a913f3f3e428
JLC <jean-luc.charles@ensam.eu>
2021-10-28 08:20:02 +0200

    add plot_object_detection_sorted_saved_model.py

--- resize_images_tt.py
+++ resize_images_tt.py
@@ -7,8 +7,8 @@ the main difference between the orignal script xxxx.py and xxxxx_tt.py
 is that the '_tt' version processes automatically the train and test
 folders assuming the tree :
 
-images/
-   |---<project_name>/
+project/
+   |---images/
             |----train/
             |      |-----*.jpg
             |      |-----*.xml
@@ -18,7 +18,7 @@ images/
             |----train_labels.csv
             |----test_labels.csv
 
-The directory <project_name> is given by the option --project.
+The directory <project> is given by the option --project.
 
 JLC v1.0 2020/07/19 initial revision of the '_tt' version.
 
@@ -29,12 +29,12 @@ from resize_images import rescale_images
 
 def main(project, size):
     for folder in ['train', 'test']:
-        image_path = os.path.join('images', project, folder)
+        image_path = os.path.join(project, 'images', folder)
         rescale_images(image_path, size)        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Rescale images")
-    parser.add_argument('-p', '--project', type=str, required=True, help='name of the directory under images/ where to find "train" and "test" images sub-directories')
+    parser.add_argument('-p', '--project', type=str, required=True, help='name of the directory containing the "images/train" and "images/test" subdirs')
     parser.add_argument('-s', '--size', type=int, nargs=2, required=True, metavar=('width', 'height'), help='Image size')
     args = parser.parse_args()
     main(args.project, args.size)
